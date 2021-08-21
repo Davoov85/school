0143505044d10ead4344437ac50bef968257c98d
Lee <lhy998095@gmail.com>
2021-08-21 11:07:07 +0900

    se

---
+++ routes/api.js
@@ -0,0 +1,35 @@
+const apiRouter = require("express").Router();
+
+const School = require('school-kr')
+const school = new School()
+
+school.init(School.Type.HIGH, School.Region.GYEONGGI, 'J100000818')
+const example = async function() {
+  const meal = await school.getMeal();
+  return(meal.today)
+  
+}
+
+
+apiRouter.post('/meal', async function(req, res) {
+  const result = await example();
+  console.log(req.body);
+
+
+  const responseBody = {
+    version: "2.0",
+    template: {
+      outputs: [
+        {
+          simpleText: {
+            text: result
+          }
+        }
+      ]
+    }
+  };
+
+  res.status(200).send(responseBody);
+});
+
+module.exports = apiRouter;
