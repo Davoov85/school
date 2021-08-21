f164a250ecd55f5583ea2ce4191367ef9453dc6d
Lee <lhy998095@gmail.com>
2021-08-21 20:12:36 +0900

    second

--- routes/api.js
+++ routes/api.js
@@ -3,33 +3,38 @@ const apiRouter = require("express").Router();
 const School = require('school-kr')
 const school = new School()
 
-school.init(School.Type.HIGH, School.Region.GYEONGGI, 'J100000818')
-const example = async function() {
-  const meal = await school.getMeal();
-  return(meal.today)
-  
-}
+school.init(School.Type.HIGH, School.Region.GYEONGGI, 'J100005286')
+// const example = async function() {
+//   const meal = await school.getMeal();
+//   return(밥)
+// }
 
-
-apiRouter.post('/meal', async function(req, res) {
-  const result = await example();
-  console.log(req.body);
-
-
-  const responseBody = {
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          simpleText: {
-            text: result
-          }
-        }
-      ]
-    }
-  };
-
-  res.status(200).send(responseBody);
+apiRouter.get('/keyboard', function(req, res) {
+   const data = {'type' : 'text'}
+   res.json(data);  
 });
 
+apiRouter.post('/meal', async function(req, res) {
+   //  const result = await example();
+   const dateInfo = req.body.userRequest.utterance; 
+
+   const meal = await school.getMeal({default: '이 날은 급식이 없습니다.'});
+   const printing = meal[24]
+   console.log(req.body);
+   
+   const responseBody = {
+     version: "2.0",
+     template: {
+       outputs: [
+         {
+           simpleText: {
+             text: `${meal.month}월 ${dateInfo}일 급식정보 \n` + printing
+           }
+         }
+       ]
+     }
+   };
+
+   res.status(200).send(responseBody);
+ });
 module.exports = apiRouter;
